8212b80e819576bdd55737500ce6e8296d5c77e5
admin <info@pebbled.io>
2019-03-19 01:29:01 +1100

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_sp_revisr.sql
+++ wp-content/uploads/revisr-backups/revisr_sp_revisr.sql
@@ -19,12 +19,12 @@ CREATE TABLE `sp_revisr` (
   `event` varchar(42) NOT NULL,
   `user` varchar(60) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `sp_revisr` WRITE;
 /*!40000 ALTER TABLE `sp_revisr` DISABLE KEYS */;
-INSERT INTO `sp_revisr` VALUES (1,'2019-03-18 14:04:01','Successfully created a new repository.','init','admin');
+INSERT INTO `sp_revisr` VALUES (1,'2019-03-18 14:04:01','Successfully created a new repository.','init','admin'),(2,'2019-03-18 14:07:54','Successfully backed up the database.','backup','admin'),(3,'2019-03-18 14:08:02','Committed <a href=\"http://wow.pebbled.io/schakraborty/softprodigy/1/wp-admin/admin.php?page=revisr_view_commit&commit=6aa2229&success=true\">#6aa2229</a> to the local repository.','commit','admin'),(4,'2019-03-18 14:08:03','Error pushing changes to the remote repository.','error','admin'),(5,'2019-03-18 14:14:55','Created new branch: test','branch','admin'),(6,'2019-03-18 14:16:14','Successfully pushed 2 commits to origin/master.','push','admin'),(7,'2019-03-18 14:16:14','Reverted to commit <a href=\"http://wow.pebbled.io/schakraborty/softprodigy/1/wp-admin/admin.php?page=revisr_view_commit&commit=6aa2229\">#6aa2229</a>.','revert','admin');
 /*!40000 ALTER TABLE `sp_revisr` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
